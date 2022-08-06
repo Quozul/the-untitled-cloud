338e11ff9fa2a38c8ea2c65bf26afd6ba8a87d20
Quozul <erwan.legloannec@outlook.com>
2022-08-06 13:59:36 +0200

    add ftp host

--- src/components/app/tabs/info/ServerFtp.svelte
+++ src/components/app/tabs/info/ServerFtp.svelte
@@ -12,7 +12,7 @@
 
 	.separation {
 		padding: 1em 0;
-        display: flex;
+		display: flex;
 		flex-direction: column;
 
 		+ .separation {
@@ -21,12 +21,12 @@
 
 		dd {
 			margin: 0;
-            flex-grow: 1;
+			flex-grow: 1;
 		}
 	}
 
 	@include media-breakpoint-up(lg) {
-        .separation {
+		.separation {
 			flex-direction: row;
 		}
 
@@ -37,36 +37,45 @@
 </style>
 
 <div class="bg-light p-4 d-flex element flex-column">
-    <h4>FTP</h4>
+	<h4>FTP</h4>
 
-    <p class="text-muted">
-        Téléchargez un client FTP de votre choix puis utilisez les informations ci-dessous pour vous connecter.
-    </p>
+	<p class="text-muted">
+		Téléchargez un client FTP de votre choix puis utilisez les informations ci-dessous pour vous connecter.
+	</p>
 
-    <dl class="d-flex flex-column m-0">
-        <div class="separation">
-            <dt>Identifiant</dt>
-            <dd>
-                L'adresse mail que vous utilisez pour vous connecter aux site.
-            </dd>
-        </div>
-        <div class="separation">
-            <dt>Mot de passe</dt>
-            <dd>
-                Le même que pour vous connecter aux site.
-            </dd>
-        </div>
-        <div class="separation">
-            {#if !server}
-                <p class="placeholder-glow w-100 m-0">
-                    <span class="placeholder h-100 col-12"></span>
-                </p>
-            {:else}
-                <dt>Répertoire</dt>
-                <dd>
-                    {server.id}
-                </dd>
-            {/if}
-        </div>
-    </dl>
+	<dl class="d-flex flex-column m-0">
+		<div class="separation">
+			<dt>Adresse du serveur FTP</dt>
+			<dd>
+				<pre class="mb-0">141.94.98.122</pre>
+			</dd>
+		</div>
+
+		<div class="separation">
+			<dt>Identifiant</dt>
+			<dd>
+				L'adresse mail que vous utilisez pour vous connecter aux site.
+			</dd>
+		</div>
+
+		<div class="separation">
+			<dt>Mot de passe</dt>
+			<dd>
+				Le même que pour vous connecter aux site.
+			</dd>
+		</div>
+
+		<div class="separation">
+			{#if !server}
+				<p class="placeholder-glow w-100 m-0">
+					<span class="placeholder h-100 col-12"></span>
+				</p>
+			{:else}
+				<dt>Répertoire</dt>
+				<dd>
+					{server.id}
+				</dd>
+			{/if}
+		</div>
+	</dl>
 </div>
--- src/routes/app/__layout.svelte
+++ src/routes/app/__layout.svelte
@@ -1,7 +1,3 @@
-<script context="module">
-	export const prerender = false;
-</script>
-
 <script>
     import Icons from "$components/icons/Icons.svelte";
 	import "$root/app.scss";
