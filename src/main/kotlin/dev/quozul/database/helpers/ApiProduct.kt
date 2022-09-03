fd2b6dee81090b11d4dc9a959de19b359e024935
Quozul <erwan.legloannec@outlook.com>
2022-09-03 12:17:33 +0200

    product specs

--- src/main/kotlin/dev/quozul/database/helpers/ApiProduct.kt
+++ src/main/kotlin/dev/quozul/database/helpers/ApiProduct.kt
@@ -1,32 +1,14 @@
 package dev.quozul.database.helpers
 
-import dev.quozul.database.models.*
 import kotlinx.serialization.Serializable
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.transactions.transaction
 
 @Serializable
 data class ApiProduct(
 	val id: String,
 	val name: String,
 	val description: String,
-) {
-	companion object {
-		// TODO: Get all products
-
-		fun fromSubscription(subscription: Subscription): List<ApiProduct> = transaction {
-			val query = SubscriptionItems.innerJoin(Products)
-				.slice(Products.columns)
-				.select { SubscriptionItems.subscription eq subscription.id }
-				.withDistinct()
-
-			Product.wrapRows(query).map {
-				ApiProduct(
-					it.id.toString(),
-					it.name,
-					it.description,
-				)
-			}
-		}
-	}
-}
+	val inStocks: Boolean,
+	val price: Int,
+	val cpu: Int,
+	val memory: Int,
+)
--- src/main/kotlin/dev/quozul/database/models/Product.kt
+++ src/main/kotlin/dev/quozul/database/models/Product.kt
@@ -1,11 +1,13 @@
 package dev.quozul.database.models
 
+import dev.quozul.database.enums.SubscriptionStatus
 import dev.quozul.database.helpers.ApiProduct
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
@@ -15,6 +17,10 @@ object Products : UUIDTable("product") {
 	val description = varchar("description", 1024)
 	val stripeId = varchar("stripe_id", 32).uniqueIndex()
 	val dockerImage = varchar("docker_image", 64) // Default image of the container
+	val price = integer("price").default(100)
+	val cpu = integer("cpu").default(1)
+	val memory = integer("memory").default(512)
+	val stocks = integer("stocks").default(1)
 	// TODO: Add default containers' limits?
 }
 
@@ -25,14 +31,34 @@ class Product(id: EntityID<UUID>) : UUIDEntity(id) {
 	var description by Products.description
 	var stripeId by Products.stripeId
 	var dockerImage by Products.dockerImage
+	var price by Products.price
+	var cpu by Products.cpu
+	var memory by Products.memory
+	var stocks by Products.stocks
 
-	fun toApiProduct(): ApiProduct {
-		return ApiProduct(
-			id.toString(),
-			name,
-			description,
-		)
-	}
+	fun toApiProduct() = ApiProduct(
+		id.toString(),
+		name,
+		description,
+		isInStocks(),
+		price,
+		cpu,
+		memory,
+	)
+
+	fun isInStocks() = usedStocks < stocks
+
+	private val usedStocks: Long
+		get() = transaction {
+			val query = SubscriptionItems.innerJoin(Subscriptions)
+				.slice(SubscriptionItems.product, Subscriptions.subscriptionStatus)
+				.select {
+					(SubscriptionItems.product eq this@Product.id) and (Subscriptions.subscriptionStatus neq SubscriptionStatus.CANCELLED)
+				}
+				.withDistinct()
+
+			SubscriptionItem.wrapRows(query).count()
+		}
 }
 
 fun getProductFromStripeId(stripeId: String): Product? = transaction {
