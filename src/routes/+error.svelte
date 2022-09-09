f0329d500f2a4bc73d2f59af3ed10c53e0e6f726
Quozul <erwan.legloannec@outlook.com>
2022-09-09 13:42:13 +0200

    not found

---
+++ src/routes/+error.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { t } from "svelte-intl-precompile";
+	import { page } from "$app/stores";
+	import Navbar from "$components/navbar/Navbar.svelte";
+	import Icon from "$components/icons/Icon.svelte";
+	import Footer from "$components/Footer.svelte";
+	import Icons from "$components/icons/Icons.svelte";
+	import Link from "$shared/Link.svelte";
+	import "$root/app.scss";
+</script>
+
+<div class="container min-vh-100 d-flex flex-column">
+	<Icons />
+	<header>
+		<Navbar/>
+	</header>
+
+	<section class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center text-center">
+		<div>
+			<div class="icon-container">
+				<Icon key="favicon" width="192" height="192"/>
+			</div>
+
+			<h1 class="display-1">{$page.status}</h1>
+			<p class="lead text-dark fs-2">
+				{$page.error.message}
+			</p>
+
+			<Link href="/" className="btn btn-primary btn-lg">{$t("home")}</Link>
+		</div>
+	</section>
+
+	<Footer/>
+</div>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,25 @@
+import {
+	addMessages,
+	init, locale,
+	waitLocale,
+} from "svelte-intl-precompile";
+import en from "$locales/en.json";
+import fr from "$locales/fr.json";
+import { defaultLocale } from "$shared/constants";
+
+/** @type {import("./$types").LayoutLoad} */
+export async function load({params}: any) {
+	addMessages("en", en);
+	addMessages("fr", fr);
+
+	init({
+		fallbackLocale: defaultLocale,
+		initialLocale: params.lang,
+	});
+
+	await waitLocale(params.lang);
+}
+
 // export const csr = true;
 // export const ssr = true;
 export const prerender = true;
