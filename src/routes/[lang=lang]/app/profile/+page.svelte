f1ae72e58cf4aa489a8f3268a0d06dd39f415e18
Quozul <erwan.legloannec@outlook.com>
2022-09-09 14:48:29 +0200

    hide device section

--- src/routes/[lang=lang]/app/profile/+page.svelte
+++ src/routes/[lang=lang]/app/profile/+page.svelte
@@ -2,8 +2,6 @@
 	import { onProfilePage, server, user } from "$store/store";
 	import Icon from "$components/icons/Icon.svelte";
 	import Button from "$shared/Button.svelte";
-	import { Variant } from "$shared/constants";
-	import * as UAParser from "ua-parser-js";
 	import { onMount } from "svelte";
 	import { getUser } from "$components/app/helpers";
 	import DeleteAccount from "$components/profile/DeleteAccount.svelte";
@@ -11,10 +9,7 @@
 	$server = null;
 	$onProfilePage = true;
 
-	let ua: UAParser.UAParserInstance;
-
 	onMount(async () => {
-		ua = new UAParser(navigator.userAgent);
 		$user = await getUser();
 	});
 </script>
@@ -63,43 +58,4 @@
 		<Button disabled>Télécharger mes données</Button>
 		<DeleteAccount/>
 	</div>
-</div>
-
-<div class="bg-light p-4 d-flex element flex-column">
-	<h4>Appareils connectés</h4>
-
-	<p class="text-muted">
-		Ci-dessous, retrouvez la liste des appareils connectés à votre compte.
-	</p>
-
-	<table class="table align-middle">
-		<thead>
-		<tr>
-			<th>Appareil</th>
-			<th>Adresse IP</th>
-			<th>Dernière connexion</th>
-			<th>Actions</th>
-		</tr>
-		</thead>
-		<tbody>
-		{#if ua}
-			<tr>
-				<td>{ua.getOS().name} {ua.getOS().version} - {ua.getBrowser().name} {ua.getBrowser().version}</td>
-				<td>---</td>
-				<td>Aujourd'hui</td>
-				<td>
-					Cet appareil
-				</td>
-			</tr>
-		{/if}
-		<tr>
-			<td>---</td>
-			<td>---</td>
-			<td>---</td>
-			<td>
-				<Button variant={Variant.DANGER} className="btn-sm" disabled>Déconnecter</Button>
-			</td>
-		</tr>
-		</tbody>
-	</table>
 </div>
